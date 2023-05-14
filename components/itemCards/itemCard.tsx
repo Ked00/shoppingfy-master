fa0f162493b9119bd9ff5a66556146a06a0d68d1
ked00 <treemansquad@gmail.com>
2023-05-14 18:45:57 -0500

    search bar and fruits category added

---
+++ components/itemCards/itemCard.tsx
@@ -0,0 +1,20 @@
+import {Card} from "@mui/joy";
+import AddIcon from "@mui/icons-material/Add";
+
+type prop = {
+  item: string,
+  category: string
+}
+
+export function ItemCard(Props: prop) {
+  return (
+    <Card
+      sx={{width: "230px", height: "60px"}}
+      orientation="horizontal"
+      className="flex justify-between"
+    >
+      <p>{Props.item}</p>
+      <AddIcon sx={{color: "#C1C1C4"}} />
+    </Card>
+  );
+}
--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,11 +1,33 @@
-import {Container} from "@mui/material";
+"use client";
+import {Input} from "@mui/joy";
+import {ItemCard} from "../../../components/itemCards/itemCard";
 
 export default function Items() {
   return (
-    <div className=" border-2 border-amber-300">
-      <Container>
-        <h1>hello</h1>
-      </Container>
+    <div className="container p-5">
+      <div className="flex">
+        <h2 className="w-2/4">
+          <span className="text-[#F9A109]">Shoppingify </span>allows you to take your shopping list
+          wherever you go
+        </h2>
+        <Input
+          placeholder="search item"
+          type="search"
+          sx={{width: "275.61px", height: "50.92px"}}
+        />
+      </div>
+
+      <div className="mt-5">
+        <h4 className="mt-3">Fruits and vegetables</h4>
+        <ItemCard item="Avocado" category="Fruits and vegetables" />
+        <ItemCard item="Banana" category="Fruits and vegetables" />
+        <ItemCard item="Bunch of carrots 5pcs" category="Fruits and vegetables" />
+        <ItemCard item="Chicken 1kg" category="Fruits and vegetables" />
+        <ItemCard item="Pre-cooked corn 450g" category="Fruits and vegetables" />
+        <ItemCard item="Mandarin Nadorcott" category="Fruits and vegetables" />
+        <ItemCard item="Piele De Sapo Melon" category="Fruits and vegetables" />
+        <ItemCard item="Watermelon" category="Fruits and vegetables" />
+      </div>
     </div>
   );
 }
