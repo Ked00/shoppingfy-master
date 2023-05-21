5d5034579af4e27a67327013423ff7ae70934207
ked00 <treemansquad@gmail.com>
2023-05-20 21:33:39 -0500

    fixed terminal error

--- components/sidemenu/AddNewItem.tsx
+++ components/sidemenu/AddNewItem.tsx
@@ -1,4 +1,4 @@
-import {Input, FormControl, FormLabel} from "@mui/joy";
+import {Input, FormControl, FormLabel, Select} from "@mui/joy";
 import {InputWithLabel} from "../Inputs/InputWithLabel";
 
 export function AddNewItem() {
@@ -9,6 +9,9 @@ export function AddNewItem() {
         <InputWithLabel label="Name" placeHolder="Enter a name" className="p-3 mb-2" />
         <InputWithLabel label="Note (optional)" placeHolder="Enter a note" className="p-14 mb-3 text-left" />
         <InputWithLabel label="image (optional)" placeHolder="Enter a url" className="p-3 mb-4" />
+        <Select placeholder="Enter a category">
+            
+        </Select>
       </form>
     </div>
   );
